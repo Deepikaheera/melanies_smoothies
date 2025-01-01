74ce19e420cb76c73cd8bdc460e5b9adff740aec
Deepikaheera <deepikadeej58@gmail.com>
2025-01-01 22:40:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,10 @@ st.write(
 
 from snowflake.snowpark.functions import col
 
-# name_on_order = st.text_input('Name on Smoothie:')
-# st.write("The name on your Smoothie will be: ", name_on_order)
+name_on_order = st.text_input('Name on Smoothie:')
+st.write("The name on your Smoothie will be: ", name_on_order)
 
-# session = get_active_session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
